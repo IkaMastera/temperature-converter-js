db63c4028eb54fb165011c90d449e9e70453c4a7
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-06-03 12:20:03 +0400

    add: celToFar function

---
+++ script.js
@@ -0,0 +1,7 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+
+function celToFar() {
+  let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+  fahrenheit.value = parseFloat(output.toFixed(2));
+}
