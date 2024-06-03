04adb3df223bb21abcc9e5be91e0e104f8e9cfb1
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-06-03 12:23:02 +0400

    add: farToCel Function
    to convert fahrenheit to celsius

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function celToFar() {
   let output = (parseFloat(celsius.value) * 9) / 5 + 32;
   fahrenheit.value = parseFloat(output.toFixed(2));
 }
+
+function farToCel() {
+  let output = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+  celsius.value = parseFloat(output.toFixed(2));
+}
